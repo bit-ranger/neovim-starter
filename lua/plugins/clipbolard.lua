2da61da2e740de87ec1524c4d4b39b7d9d6ce4ee
bit-ranger <sincerebravefight@gmail.com>
2025-05-19 20:52:07 +0800

    clipboard

---
+++ lua/plugins/clipbolard.lua
@@ -0,0 +1,28 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+
+  -- add tsserver and setup with typescript.nvim instead of lspconfig
+  {
+    "ojroques/nvim-osc52",
+    opts = function(_, opts)
+      local function copy(lines, _)
+        require("osc52").copy(table.concat(lines, "\n"))
+      end
+
+      local function paste()
+        return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+      end
+
+      vim.g.clipboard = {
+        name = "osc52",
+        copy = { ["+"] = copy, ["*"] = copy },
+        paste = { ["+"] = paste, ["*"] = paste },
+      }
+    end,
+  },
+}
