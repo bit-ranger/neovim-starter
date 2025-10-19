c59194d3d34a5adb33580b6ac6387e17592a1423
bit-ranger <sincerebravefight@gmail.com>
2025-10-19 14:04:20 +0800

    remove clipboard

--- lua/plugins/clipbolard.lua
+++ lua/plugins/clipbolard.lua
@@ -7,22 +7,22 @@
 return {
 
   -- add tsserver and setup with typescript.nvim instead of lspconfig
-  {
-    "ojroques/nvim-osc52",
-    opts = function(_, opts)
-      local function copy(lines, _)
-        require("osc52").copy(table.concat(lines, "\n"))
-      end
-
-      local function paste()
-        return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-      end
-
-      vim.g.clipboard = {
-        name = "osc52",
-        copy = { ["+"] = copy, ["*"] = copy },
-        paste = { ["+"] = paste, ["*"] = paste },
-      }
-    end,
-  },
+  -- {
+  --   "ojroques/nvim-osc52",
+  --   opts = function(_, opts)
+  --     local function copy(lines, _)
+  --       require("osc52").copy(table.concat(lines, "\n"))
+  --     end
+  --
+  --     local function paste()
+  --       return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+  --     end
+  --
+  --     vim.g.clipboard = {
+  --       name = "osc52",
+  --       copy = { ["+"] = copy, ["*"] = copy },
+  --       paste = { ["+"] = paste, ["*"] = paste },
+  --     }
+  --   end,
+  -- },
 }
